93fe0f62db95ea9603cb3208680d29c2eb5f9134
John Erling Blad <jeblad@gmail.com>
2025-12-16 06:26:25 +0100

    Change to templated class

--- src/hash-fnv1a.h
+++ src/hash-fnv1a.h
@@ -4,41 +4,96 @@ Fowler–Noll–Vo 1a (or FNV1a) non-cryptographic hash function
 
 This is a pretty stright-forward implementation of the hash function
 
-TODO: It should probably be implemented as a template that specializes
-      for a few parametrized primes. The offset might be recalculated
-      for each instance.
-
 */
 
 #pragma once
 
 #include <string>
 #include <cstdint>
-#include <optional>
 #include <concepts>
-
-#define NO_FNV_GCC_OPTIMIZATION false
+#include <iostream>
 
 namespace hash{
 
-    uint32_t fnv1a(
-        std::string str,
-        std::optional<uint32_t> divisor=std::nullopt
-    ) {
-        const uint32_t offset = 2166136261;
-        const uint32_t prime = 16777619;
+    template<std::unsigned_integral T, uint64_t prime, uint64_t offset=0>
+    class FNV1a {
+        private:
+        const T _prime{static_cast<T>(prime)};
+        T _offset{0};
 
-        uint32_t hash = offset;
+        public:
 
-            for (unsigned int i = 0; i < str.length(); i++) {
-                hash ^= (uint32_t) str[i];
-#if defined(NO_FNV_GCC_OPTIMIZATION)
-                hash *= prime;
-#else
-                hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
-#endif
+        FNV1a() {
+            std::cout << "generalization" << "\n";
+            if (offset) {
+                _offset = static_cast<T>(offset);
             }
-        return divisor.has_value() ? (hash % divisor.value()) : hash;
-    }
+            else {
+                _offset = hash("chongo <Landon Curt Noll> /\\../\\");
+            }
+        }
+
+        T hash(
+            std::string str
+        ) {
+
+            T _hval = _offset;
+            for (unsigned int i = 0; i < str.length(); i++) {
+                _hval ^= (T) str[i];
+                _hval *= _prime;
+            }
+            return _hval;
+        }
+    };
+
+    #if !defined(NO_FNV_GCC_OPTIMIZATION)
+
+    template<std::unsigned_integral T>
+    class FNV1a<T, 16777619ULL, 2166136261ULL> {
+        private:
+        const T _offset{static_cast<T>(2166136261ULL)};
+
+        public:
+
+        FNV1a() {
+            std::cout << "spesialization" << "\n";
+        }
+
+        T hash(
+            std::string str
+        ) {
+            T _hval = _offset;
+            for (unsigned int i = 0; i < str.length(); i++) {
+                _hval ^= (T) str[i];
+                _hval += (_hval << 1) + (_hval << 4) + (_hval << 7)
+                    + (_hval << 8) + (_hval << 24);
+            }
+            return _hval;
+        }
+    };
+
+    template<std::unsigned_integral T>
+    class FNV1a<T, 1099511628211ULL, 14695981039346656037ULL> {
+        private:
+        const T _offset{static_cast<T>(14695981039346656037ULL)};
+
+        public:
+
+        FNV1a() {}
+
+        T hash(
+            std::string str
+        ) {
+            T _hval = _offset;
+            for (unsigned int i = 0; i < str.length(); i++) {
+                _hval ^= (T) str[i];
+                _hval += (_hval << 1) + (_hval << 4) + (_hval << 5)
+                    + (_hval << 7) + (_hval << 8) + (_hval << 40);
+            }
+            return _hval;
+        }
+    };
+
+    #endif
 
 }
--- test/hash-fnv1a-test.cpp
+++ test/hash-fnv1a-test.cpp
@@ -2,23 +2,41 @@
   Testing Procedures for Fowler–Noll–Vo 1a hash function
 */
 
+#include <cstdint>
+
 #include "hash-fnv1a.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("testing the hash function for repeatability") {
+TEST_CASE("testing the general hash function for repeatability") {
+    // this use the alternate less-than prime for 32 bit
+    hash::FNV1a<uint32_t, 16777499ULL> fnv1a{};
     const std::string str1 = "this is the test string 1";
     const std::string str2 = "this is the test string 2";
-    CHECK(hash::fnv1a("") == 2166136261);
-    CHECK(hash::fnv1a(str1) == 2380602814);
-    CHECK(hash::fnv1a(str2) == 2363825195);
-    CHECK(hash::fnv1a(str1) != hash::fnv1a(str2));
+    CHECK(fnv1a.hash("") == 2230366783);
+    CHECK(fnv1a.hash(str1) == 1452757912);
+    CHECK(fnv1a.hash(str2) == 1503090409);
+    CHECK(fnv1a.hash(str1) != fnv1a.hash(str2));
 }
 
+TEST_CASE("testing the special hash function for repeatability") {
+    // this use the IEEE POSIX P1003.2 proposal greater-than prime for 32 bit
+    hash::FNV1a<uint32_t, 16777619ULL, 2166136261ULL> fnv1a{};
+    const std::string str1 = "this is the test string 1";
+    const std::string str2 = "this is the test string 2";
+    CHECK(fnv1a.hash("") == 2166136261);
+    CHECK(fnv1a.hash(str1) == 2380602814);
+    CHECK(fnv1a.hash(str2) == 2363825195);
+    CHECK(fnv1a.hash(str1) != fnv1a.hash(str2));
+}
 
-TEST_CASE("testing the hash function's modulo operation'") {
-    const std::string str = "this is the test string 1";
-    CHECK(hash::fnv1a(str) == 2380602814);
-    CHECK(hash::fnv1a(str, 0xff) == 79);
+TEST_CASE("testing the special hash function for repeatability") {
+    hash::FNV1a<uint64_t, 1099511628211ULL, 14695981039346656037ULL> fnv1a{};
+    const std::string str1 = "this is the test string 1";
+    const std::string str2 = "this is the test string 2";
+    CHECK(fnv1a.hash("") == 14695981039346656037ULL);
+    CHECK(fnv1a.hash(str1) == 12073451778302911390ULL);
+    CHECK(fnv1a.hash(str2) == 12073450678791283179ULL);
+    CHECK(fnv1a.hash(str1) != fnv1a.hash(str2));
 }
